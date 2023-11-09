72c3adf01f2d0157a4ba8bddcb86f85c440479d1
Adham Ahmed <adham.ahmed@bld.ai>
2023-11-09 17:48:00 +0200

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,6 +21,7 @@ const useGames = (GameQuery: GameQuery) =>
       params: {
         genres: GameQuery.genre?.id,
         platforms: GameQuery.platform?.id,
+        ordering: GameQuery.sortOrder,
       },
     },
     [GameQuery]
